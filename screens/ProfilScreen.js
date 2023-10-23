e884bd041d52baa5b0b80773ef78f007f8f9b1d1
Valène Bonnet <bonnet.valene@gmail.com>
2023-10-23 20:28:28 +0200

    navigation & map

---
+++ screens/ProfilScreen.js
@@ -0,0 +1,10 @@
+import { Button, StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function ProfileScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Profile Screen</Text>
+    </SafeAreaView>
+  );
+}
