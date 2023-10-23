561b6047b26a35863bbfb5e39f1ecff3f568a026
charlieboudou <charlie.lcb3@gmail.com>
2023-10-23 09:09:25 +0200

    login

---
+++ components/Input.js
@@ -0,0 +1,51 @@
+import { View, Text, TextInput } from 'react-native';
+import tw from 'twrnc';
+
+export default function Input({
+ value,
+ setValue,
+ placeholder,
+ border,
+ size,
+ error,
+ setError,
+}) {
+ const inputHeight = () => {
+    switch (size) {
+    default:
+    case "normal":
+        return "h-[2.5rem] mb-[.6rem]";
+    case "small":
+        return "h-[2rem] mb-[.6rem]";
+    case "large":
+        return "h-[3rem] mb-[1.6rem]";
+    };
+ };
+
+ const errorMessages = {
+    Email: 'Veuillez saisir une adresse mail valide.',
+    Password: 'Mot de passe incorrect.',
+    'Confirm Password': 'Le mot de passe est différent.',
+    Firstname: 'Veuillez saisir un prénom.',
+    Lastname: 'Veuillez saisir un nom de famille.',
+    Username: 'Pseudo incorrect.',
+ };
+
+ return (
+    <View style={tw`w-[80%]`}>
+        <TextInput
+        style={tw`${border ? 'border border-black' : ''} ${size === "large" ? 'text-center' : 'pl-[.5rem]'} bg-[#D9D9D9] text-black rounded-[.625rem] ${inputHeight()}`}
+        placeholder={placeholder}
+        onChangeText={(value) => {
+            if(error) {
+                setError('');
+            }
+            setValue(value);
+        }} 
+        value={value}
+        secureTextEntry={placeholder === "Password" || placeholder === "Confirm Password"}
+        />
+        {error && error === placeholder &&  <Text style={tw`text-red-700 pb-[.5rem]`}>*{errorMessages[error]}</Text>}
+    </View>
+ );
+}
