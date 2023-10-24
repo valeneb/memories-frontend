6ae1d85cd023c4ded746361a341ee07536310ff7
charlieboudou <charlie.lcb3@gmail.com>
2023-10-24 11:42:36 +0200

    reducers

--- App.js
+++ App.js
@@ -2,17 +2,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet, View } from 'react-native';
-// import { Provider } from 'react-redux';
-//import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProfilScreen from './screens/ProfilScreen';
 import SearchScreen from './screens/SearchScreen';
 
-// const store = configureStore({
-//   reducer: { user },
-// });
+const store = configureStore({
+  reducer: { user },
+});
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -69,59 +69,59 @@ const TabNavigator = () => {
 
 export default function App() {
   return (
-    // <Provider store={store}>
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ color, size }) => {
-            let iconName = '';
-            let iconImage = null;
+    <Provider store={store}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              let iconName = '';
+              let iconImage = null;
 
-            if (route.name === 'Map') {
-              iconName = 'globe';
-            } else if (route.name === 'Profil') {
-              iconName = 'user';
-            } else if (route.name === 'Search') {
-              iconName = 'search';
-            }
+              if (route.name === 'Map') {
+                iconName = 'globe';
+              } else if (route.name === 'Profil') {
+                iconName = 'user';
+              } else if (route.name === 'Search') {
+                iconName = 'search';
+              }
 
-            return (
-              <FontAwesome
-                name={iconName}
-                size={size + 8}
-                color={color}
-                style={{ marginBottom: -16 }}
-              />
-            );
-          },
-          tabBarActiveTintColor: '#fff',
-          tabBarInactiveTintColor: 'rgba(255,255,255,0.5)',
-          tabBarStyle: { backgroundColor: '#D8725B' },
-          headerShown: false,
-        })}
-      >
-        <Tab.Screen
-          name="Map"
-          component={HomeScreen}
-          options={{ tabBarLabel: '' }}
-        />
-        <Tab.Screen
-          name="Profil"
-          component={ProfilScreen}
-          options={{ tabBarLabel: '' }}
-        />
-        <Tab.Screen
-          name="Search"
-          component={SearchScreen}
-          options={{ tabBarLabel: '' }}
-        />
-      </Tab.Navigator>
-      {/* <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="TabNavigator" component={TabNavigator} />
-      </Stack.Navigator> */}
-    </NavigationContainer>
-    // </Provider>
+              return (
+                <FontAwesome
+                  name={iconName}
+                  size={size + 8}
+                  color={color}
+                  style={{ marginBottom: -16 }}
+                />
+              );
+            },
+            tabBarActiveTintColor: '#fff',
+            tabBarInactiveTintColor: 'rgba(255,255,255,0.5)',
+            tabBarStyle: { backgroundColor: '#D8725B' },
+            headerShown: false,
+          })}
+        >
+          <Tab.Screen
+            name="Map"
+            component={HomeScreen}
+            options={{ tabBarLabel: '' }}
+          />
+          <Tab.Screen
+            name="Profil"
+            component={ProfilScreen}
+            options={{ tabBarLabel: '' }}
+          />
+          <Tab.Screen
+            name="Search"
+            component={SearchScreen}
+            options={{ tabBarLabel: '' }}
+          />
+        </Tab.Navigator>
+        {/* <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="TabNavigator" component={TabNavigator} />
+        </Stack.Navigator> */}
+      </NavigationContainer>
+    </Provider>
   );
 }
 
