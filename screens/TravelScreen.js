007b302857a377b0bc062119482bc8315bb5ae72
charlieboudou <charlie.lcb3@gmail.com>
2023-10-25 11:30:51 +0200

    header et page travelScreen

--- screens/TravelScreen.js
+++ screens/TravelScreen.js
@@ -1,18 +1,18 @@
-import { Button, StyleSheet, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, View } from 'react-native';
 import Header from '../components/Header';
 import { useState } from 'react';
+import tw from 'twrnc';
 
-export default function ProfileScreen() {
+export default function TravelScreen() {
   const [isDairyActive, setIsDairyActive] = useState(false);
 
   const onClick = (title) => {
     console.log('title', title);
   };
   return (
-    <SafeAreaView>
+    <View style={tw`bg-[#F2DDC2] w-full h-full`}>
       <Header title="Brazil" icon="edit" onClick={onClick} isDairyActive={isDairyActive} setIsDairyActive={setIsDairyActive} />
       <Text>Profile Screen</Text>
-    </SafeAreaView>
+    </View>
   );
 }
