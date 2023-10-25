2ecd310c88daa8d4114937e4bc8fe9d62dedd200
Valène Bonnet <bonnet.valene@gmail.com>
2023-10-25 19:42:45 +0200

    trame du planning modal

--- components/Header.js
+++ components/Header.js
@@ -2,27 +2,47 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import tw from 'twrnc';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-export default function Header({title, icon, onClick, isDairyActive, setIsDairyActive}) {
- return (
+export default function Header({
+  title,
+  icon,
+  onClick,
+  isDairyActive,
+  setIsDairyActive,
+}) {
+  return (
     <View style={tw`w-full`}>
-        <View style={tw`flex flex-row items-center justify-between bg-[#D8725B] px-[1rem] pt-[2rem] pb-[1rem]`}>
-            <Text style={tw`text-[#F2DDC2] text-[2rem]`}>{title}</Text>
-            <TouchableOpacity onPress={onClick}>
-                <FontAwesome
-                    name={icon}
-                    size={32}
-                    color="#F2DDC2"
-                />
-            </TouchableOpacity>
-        </View>
-        <View style={tw`flex flex-row items-center`}>
-            <TouchableOpacity style={tw`w-[50%] p-[.5rem] flex items-center ${isDairyActive ? 'bg-[#D3C3AE] border-b border-r border-black' : 'bg-[#F2DDC2]'}`} onPress={() => setIsDairyActive(!isDairyActive)}>
-                <Text>Programme</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={tw`w-[50%] p-[.5rem] flex items-center ${isDairyActive ? 'bg-[#F2DDC2]' : 'bg-[#D3C3AE] border-b border-l border-black'}`} onPress={() => setIsDairyActive(!isDairyActive)}>
-                <Text>Carnet de voyage</Text>
-            </TouchableOpacity>
-        </View>
+      <View
+        style={tw`flex flex-row items-center justify-between bg-[#D8725B] px-[1rem] pt-[2rem] pb-[1rem]`}
+      >
+        <Text style={tw`text-[#F2DDC2] text-[2rem]`}>{title}</Text>
+        {icon && onClick && (
+          <TouchableOpacity onPress={onClick}>
+            <FontAwesome name={icon} size={32} color="#F2DDC2" />
+          </TouchableOpacity>
+        )}
+      </View>
+      <View style={tw`flex flex-row items-center`}>
+        <TouchableOpacity
+          style={tw`w-[50%] p-[.5rem] flex items-center ${
+            isDairyActive
+              ? 'bg-[#D3C3AE] border-b border-r border-black'
+              : 'bg-[#F2DDC2]'
+          }`}
+          onPress={() => setIsDairyActive(!isDairyActive)}
+        >
+          <Text>Programme</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={tw`w-[50%] p-[.5rem] flex items-center ${
+            isDairyActive
+              ? 'bg-[#F2DDC2]'
+              : 'bg-[#D3C3AE] border-b border-l border-black'
+          }`}
+          onPress={() => setIsDairyActive(!isDairyActive)}
+        >
+          <Text>Carnet de voyage</Text>
+        </TouchableOpacity>
+      </View>
     </View>
- );
+  );
 }
--- screens/TravelScreen.js
+++ screens/TravelScreen.js
@@ -2,16 +2,20 @@ import { Text, View } from 'react-native';
 import { useState } from 'react';
 import tw from 'twrnc';
 import Diary from '../components/diary/Diary';
+import Planning from '../components/planning/Planning';
 
 export default function TravelScreen() {
-  const [isDairyActive, setIsDairyActive] = useState(true);
-  
+  const [isDairyActive, setIsDairyActive] = useState(false);
+
   return (
     <View style={tw`bg-[#F2DDC2] w-full h-full`}>
       {isDairyActive ? (
-        <Diary isDairyActive={isDairyActive} setIsDairyActive={setIsDairyActive} />
+        <Diary
+          isDairyActive={isDairyActive}
+          setIsDairyActive={setIsDairyActive}
+        />
       ) : (
-        <Text>Program Screen</Text>
+        <Planning />
       )}
     </View>
   );
