b8b924ce34986745572dc093491cc68ac059a78e
Valène Bonnet <bonnet.valene@gmail.com>
2023-10-31 19:05:35 +0100

    Design ProfilScreen

--- screens/ProfilScreen.js
+++ screens/ProfilScreen.js
@@ -7,58 +7,157 @@ import Input from '../components/Input';
 
 export default function ProfileScreen() {
   const user = useSelector((state) => state.user.value);
-
   const [isEdit, setIsEdit] = useState(false);
+  const [isSelect, setIsSelect] = useState(false);
   const [username, setUsername] = useState(user.account.username);
   const [firstname, setFirstname] = useState(user.firstname);
   const [lastname, setLastname] = useState(user.lastname);
   const [email, setEmail] = useState(user.email);
- 
+
+  const handleUpdate = () => {
+    setIsEdit(!isEdit);
+  };
+
+  const handleSelect = () => {
+    setIsSelect(!isSelect);
+  };
+
+  const handleDelete = () => {
+    if (!isSelect) {
+      alert("La case n'est pas cochée");
+    } else {
+      console.log('Gérer la suppression ici');
+    }
+  };
+
   console.log('user', user);
+
   return (
     <View style={tw`w-full h-full bg-[#D8725B] pt-[6rem] flex items-center`}>
       <View style={tw`w-full flex flex-col items-center`}>
-        <TouchableOpacity style={tw`p-[2rem] bg-white/60 rounded-full flex items-center`}>
+        <TouchableOpacity
+          style={tw`p-[2rem] bg-white/60 rounded-full flex items-center`}
+        >
           <FontAwesome name="user" size={48} />
         </TouchableOpacity>
-        <TouchableOpacity style={tw`flex items-center`}>
-          <TextInput style={tw`${isEdit ? '' : 'text-bold'} my-[1rem] text-white text-[1rem]`} value={username} editable={isEdit} onChangeText={(value) => setUsername(value)}/>
+        <TouchableOpacity style={tw`w-[70%] flex items-center`}>
+          <TextInput
+            style={tw`${
+              isEdit ? 'border-b-2 border-[#e49c8c]' : 'font-bold'
+            } my-[1rem] text-white text-[1rem]`}
+            value={username}
+            editable={isEdit}
+            onChangeText={(value) => setUsername(value)}
+          />
         </TouchableOpacity>
       </View>
-      <View style={tw`mt-[1rem]`}>
-        <Text style={tw`text-[1.3rem] text-white mb-[1.5rem]`}>Mes informations personnelles</Text>
-        <View stylz={tw`w-full flex flex-col items-center`}>
-          <Input 
-            value={firstname}
-            setValue={setFirstname}
-            placeholder="Prénom"
-            size="normal"
-          />
-          <Input 
-            value={lastname}
-            setValue={setLastname}
-            placeholder="Nom de famille"
-            size="normal"
-          />
-          <Input 
-            value={email}
-            setValue={setEmail}
-            placeholder="Email"
-            size="normal"
-          />
-        </View>
-      </View>
+
       <View style={tw`mt-[1rem] w-full flex items-center flex-col`}>
-        <Text style={tw`text-[1.3rem] text-white mb-[1.5rem]`}>Supprimer mon compte</Text>
-        <View style={tw`flex flex-row items-center justify-between w-full px-[1rem] mb-[1rem]`}>
-          <Text style={tw`mr-[.5rem]`}>Check</Text>
+        <Text style={tw`text-[1.3rem] text-white mb-[1.25rem]`}>
+          Mes informations personnelles
+        </Text>
+        <View style={tw`w-[70%] flex flex-col items-center`}>
+          {isEdit ? (
+            <Input
+              value={firstname}
+              setValue={setFirstname}
+              placeholder="Prénom"
+              size="normal"
+            />
+          ) : (
+            <Text
+              style={[
+                tw`w-[80%] h-2.5rem bg-[#e49c8c] text-black rounded-[.625rem] pl-[.5rem] mb-[.6rem] text-[0.9rem] p-2`,
+                { color: '#073040' },
+                { textAlignVertical: 'center' },
+              ]}
+            >
+              {firstname}
+            </Text>
+          )}
+
+          {isEdit ? (
+            <Input
+              value={lastname}
+              setValue={setLastname}
+              placeholder="Nom de famille"
+              size="normal"
+            />
+          ) : (
+            <Text
+              style={[
+                tw`w-[80%] h-2.5rem bg-[#e49c8c] text-black rounded-[.625rem] pl-[.5rem] mb-[.6rem] text-[0.9rem] p-2`,
+                { color: '#073040' },
+                { textAlignVertical: 'center' },
+              ]}
+            >
+              {lastname}
+            </Text>
+          )}
+
+          {isEdit ? (
+            <Input
+              value={email}
+              setValue={setEmail}
+              placeholder="Email"
+              size="normal"
+            />
+          ) : (
+            <Text
+              style={[
+                tw`w-[80%] h-2.5rem bg-[#e49c8c] text-black rounded-[.625rem] pl-[.5rem] mb-[.6rem] text-[0.9rem] p-2`,
+                { color: '#073040' },
+                { textAlignVertical: 'center' },
+              ]}
+            >
+              {email}
+            </Text>
+          )}
+        </View>
+
+        <TouchableOpacity
+          onPress={() => handleUpdate()}
+          style={tw`w-[80%] flex flex-row items-center justify-center bg-[#073040] text-white rounded-[.625rem] p-[.625rem]`}
+        >
+          <FontAwesome
+            name={isEdit ? 'check' : 'edit'}
+            size={24}
+            style={tw`text-white mr-4`}
+          />
+          <Text style={tw`text-white`}>{isEdit ? 'Valider' : 'Modifier'}</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={tw`mt-[2rem] w-full flex items-center flex-col`}>
+        <Text style={tw`text-[1.3rem] text-white mb-[1.25rem]`}>
+          Supprimer mon compte
+        </Text>
+        <View
+          style={tw`flex flex-row items-center justify-between w-full px-[1rem] mb-[1rem]`}
+        >
+          <TouchableOpacity
+            onPress={() => handleSelect()}
+            style={tw`flex items-center justify-center m-auto ${
+              isSelect ? 'bg-[#073040] text-white' : 'bg-[#D9D9D9] w-8 h-8' //peut-être revoir w & h, je te laisse voir Charlie
+            } rounded-[.625rem] p-2`}
+          >
+            <FontAwesome
+              name={isSelect ? 'check' : ''}
+              size={16}
+              style={tw`${isSelect ? 'text-white' : ''}`}
+            />
+          </TouchableOpacity>
+
           <Text style={tw`w-[85%] text-justify`}>
-            Je confirme vouloir supprimer mon compte.
-            Cette action est irréversible. 
-            Toutes les données transmise seront supprimées de nos serveurs.
+            Je confirme vouloir supprimer mon compte. Cette action est
+            irréversible. Toutes les données transmises seront supprimées de nos
+            serveurs.
           </Text>
         </View>
-        <TouchableOpacity style={tw`w-[80%] flex items-center bg-[#D9D9D9] rounded-[.625rem] p-[.625rem]`}>
+        <TouchableOpacity
+          onPress={() => handleDelete()}
+          style={tw`w-[80%] flex items-center bg-[#D9D9D9] rounded-[.625rem] p-[.625rem]`}
+        >
           <Text style={tw`text-black`}>Confirmer la suppression du compte</Text>
         </TouchableOpacity>
       </View>
